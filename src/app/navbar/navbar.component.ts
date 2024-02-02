44b8a30cf8c79ec8a9fe90e7d847a76e9a6a34e1
alen <alenbogadi360@gmail.com>
2024-02-02 16:09:53 +0100

    Add navbar

---
+++ src/app/navbar/navbar.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { AuthService } from '../auth.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  imports: [],
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.css',
+})
+export class NavbarComponent {
+  constructor(private authService: AuthService, private router: Router) {}
+  onLogout() {
+    this.authService.logout();
+    this.router.navigate(['login']);
+  }
+}
