cefdfdde9a9ca6d912e198cb2cb62f68574aaec3
alen <alenbogadi360@gmail.com>
2024-02-05 16:39:47 +0100

    Add category and thread routes to front end

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,14 @@ import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { ForumComponent } from './forum/forum.component';
 import { authGuard } from './auth.guard';
+import { CategoryComponent } from './category/category.component';
+import { ThreadComponent } from './thread/thread.component';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'register', component: RegisterComponent },
   { path: 'forum', component: ForumComponent, canActivate: [authGuard] },
+  { path: 'forum/:category_id', component: CategoryComponent },
+  { path: 'forum/:category_id/:thread_id', component: ThreadComponent },
 ];
