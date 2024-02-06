75a200031d3d9ebc8bc563560cf934be6a1e837d
alen <alenbogadi360@gmail.com>
2024-02-06 23:18:51 +0100

    Add user role check in navbar component

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,16 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../auth.service';
 import { Router, RouterLink } from '@angular/router';
+import { User } from '../models/user.model';
+import { Observable } from 'rxjs';
+import { AsyncPipe, JsonPipe } from '@angular/common';
+import { JsonpClientBackend } from '@angular/common/http';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, AsyncPipe, JsonPipe],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css',
 })
 export class NavbarComponent {
-  constructor(private authService: AuthService, private router: Router) {}
+  user = new Observable<User | null>();
+  constructor(private authService: AuthService, private router: Router) {
+    this.user = this.authService.user$;
+  }
   onLogout() {
     this.authService.logout();
     this.router.navigate(['login']);
