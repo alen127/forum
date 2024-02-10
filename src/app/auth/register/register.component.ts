842d91b9ba5e7e4a7c147ed667db38378a9b51b5
alen <alenbogadi360@gmail.com>
2024-02-10 14:53:26 +0100

    Add password confirmation field to registration form

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -14,6 +14,7 @@ import { User } from '../../shared/models/user.model';
 export class RegisterComponent {
   constructor(private authService: AuthService, private router: Router) {}
   user: Partial<User> = {};
+  checkPassword: string = '';
   registerSucess = false;
   message = '';
   onRegister() {
